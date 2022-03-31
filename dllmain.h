f0fdcca1ee344e773320cf01df286f8e97fe0185
孙振国 <sunzhenguo@live.com>
2022-03-31 09:27:31 +0800

    初始提交，20个文件

---
+++ dllmain.h
@@ -0,0 +1,10 @@
+﻿// dllmain.h: 模块类的声明。
+
+class CTestModule : public ATL::CAtlDllModuleT< CTestModule >
+{
+public :
+	DECLARE_LIBID(LIBID_TestLib)
+	DECLARE_REGISTRY_APPID_RESOURCEID(IDR_TEST, "{c255a35d-567b-4c61-b395-c2dba61a086b}")
+};
+
+extern class CTestModule _AtlModule;
